a781234019c4d59f59cccfcf18aa64c0f867bce5
magdalena-ob <you@example.com>
2022-03-15 17:42:42 +0100

    show added tasks at dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { MatDialog } from '@angular/material/dialog';
+import { Task } from 'src/models/task.class';
 import { DialogAddTaskComponent } from '../dialog-add-task/dialog-add-task.component';
 
 @Component({
@@ -9,10 +10,20 @@ import { DialogAddTaskComponent } from '../dialog-add-task/dialog-add-task.compo
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  task = new Task();
+  allTasks: any = [];
 
   constructor(public dialog: MatDialog, private firestore: AngularFirestore) { }
 
   ngOnInit(): void {
+    this.firestore
+      .collection('tasks')
+      .valueChanges()
+      .subscribe((changes) => {
+        this.allTasks = changes;
+        console.log('all tasks are ', this.allTasks);
+      });
+
   }
 
   openDialog() {
