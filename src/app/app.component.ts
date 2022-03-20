36ebd9fcda29488026353aa7c1e1b0866a94f4fa
magdalena-ob <you@example.com>
2022-03-20 16:01:07 +0100

    sidenav responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { Component, ViewChild } from '@angular/core';
+import { MatDrawer } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'simple-crm';
+
+  @ViewChild(MatDrawer) 
+    drawer!: MatDrawer;
+  
+
+  constructor(private observer: BreakpointObserver) {
+
+  }
+
+  ngAfterViewInit() {
+    this.observer.observe(['(max-width: 900px)']).subscribe((res) => {
+      if(res.matches) {                         //for mobile
+        this.drawer.mode = 'over';
+        this.drawer.close();
+      } else {
+        this.drawer.mode = 'side';
+        this.drawer.open();
+      }
+    });
+  }
 }
